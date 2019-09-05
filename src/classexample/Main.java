028e4e74e631fd3b906b258a1b369d986a802061
prudhvi2914 <prudhvi.satram1995@gmail.com>
2019-09-05 11:19:14 -0400

    initial commit

---
+++ src/classexample/Main.java
@@ -0,0 +1,20 @@
+package classexample;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		//take input
+		
+		System.out.println("enter your input");
+		Scanner keyboard = new Scanner(System.in);
+		String personName = keyboard.nextLine();
+				//output a wlcome message
+				
+		System.out.println("Hello" + personName);
+	}
+
+}
